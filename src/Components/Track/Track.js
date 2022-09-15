aa9823674e294015353dbc62b72b920e1efb0808
Alexander K <jonhy.blaze@gmail.com>
2022-09-15 18:24:12 +0300

    adding hardcoded playlist functionality

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,14 +4,28 @@ import './Track.css'
 
 class Track extends React.Component {
   
-  renderAction() {
-    if (this.props.isRemoval) {
-      return <button className="Track-action">-</button>
-    } else { 
-    return <button className="Track-action">+</button>
-    }
+  constructor(props) {
+    super(props)
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
   
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action" onClick={this.removeTrack}>-</button>
+    } else { 
+    return <button className="Track-action" onClick={this.addTrack}>+</button>
+    }
+  }
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+  removeTrack () {
+    this.props.onRemove(this.props.track)
+  }
+
   render () {
     return (
       <div className="Track">
